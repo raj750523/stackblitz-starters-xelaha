1f11164de6dacfd3885fe4658e341bb5e0f724de
raj750523 <raj750523@gmail.com>
2024-02-01 15:57:47 +0530

    sadad

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require("express"); 
-const app = express(); app.get("/", (req, res) => { 
-  res.send("Express on Vercel"); });
-   const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => { 
-      console.log(`Server is running on port ${PORT}`);
+const axios = require("axios")
+const app = express(); 
+app.get("/", (req, res) => { 
+  res.send("Express on Vercel");
+  const url =
+  'https://linkredirect.in/visitproduct/1234/PPS16-Myntra-catcard204?id=3217370&shareid=ngSXdVN';
+  let startPattern = "'exitID': '";
+  let endPattern = "'";
+  axios
+    .get(url)
+    .then((response) => {
+      console.log('Response:', response.data);
+      let html = response.data;
+      let startIndex = html.indexOf(startPattern);
+      let endIndex = html.indexOf(endPattern, startIndex + startPattern.length);
+      if (startIndex != -1 && endIndex != -1) {
+        let extractedSubstring = html.substring(
+          startIndex + startPattern.length,
+          endIndex
+        );
+        console.log(extractedSubstring);
+      } else {
+        console.log('Substring not found.');
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+    });
+
+
 });
+   
+app.post("/getExitID", function(req, res){
+  //this is a callback function
+  res.send(req)
+})
